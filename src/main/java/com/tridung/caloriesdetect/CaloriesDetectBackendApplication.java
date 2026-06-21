73e68ef34f26cabccc5c329bb03c508a1e4261fc
tridung666 <tridung1262004@gmail.com>
2026-06-21 17:45:33 +0700

    Initial project

---
+++ src/main/java/com/tridung/caloriesdetect/CaloriesDetectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tridung.caloriesdetect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaloriesDetectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaloriesDetectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tridung/caloriesdetect/CaloriesDetectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tridung.caloriesdetect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaloriesDetectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
